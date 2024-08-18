c860703b7919c12d70c3f87fdee7ee89e360117c
Emran <nightsking12345@gmail.com>
2024-08-18 18:09:20 +0500

    Добавил модуль fortune и стили для него в файле style.css

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
+import { menu } from './constants/contsants'
 import './styles.css'
-import { ContextMenu } from './menu'
 
 import { BackgroundModule } from './modules/background.module'
 import { ClicksModule } from './modules/clicks.module'
-import { ShapeModule } from './modules/shape.module'
+import { FortuneCookies } from './modules/fortuneСookies.module'
 import { HeadingModule } from './modules/heading.module'
+import { ShapeModule } from './modules/shape.module'
 
-const menu = new ContextMenu('#menu')
-
+// Menu
 menu.add(new BackgroundModule())
 menu.add(new ClicksModule())
 menu.add(new ShapeModule())
 menu.add(new HeadingModule())
+menu.add(new FortuneCookies())
--- src/modules/fortuneСookies.module.js
+++ src/modules/fortuneСookies.module.js
@@ -1,9 +1,65 @@
+import { BASE_URL_API } from '../constants/contsants'
 import { Module } from '../core/module'
+import { random } from '../utils'
 
 export class FortuneCookies extends Module {
 	constructor() {
 		super('fortune', 'Fortune Cookies')
 	}
 
-	trigger() {}
+	async fetchFortuneData() {
+		try {
+			const response = await fetch(BASE_URL_API)
+
+			if (!response.ok) {
+				throw new Error('Error fetching data')
+			}
+
+			return await response.json()
+		} catch (error) {
+			console.error(`Error: ${error.message}`)
+			return []
+		} finally {
+			console.log('Fetch completed')
+		}
+	}
+
+	async trigger() {
+		const containerFortune = document.createElement('div')
+		const containerRow = document.createElement('div')
+		const titleFortune = document.createElement('h1')
+		const btnFortune = document.createElement('button')
+		const randomTitle = document.createElement('h2')
+
+		// Content
+		titleFortune.textContent = 'Fortune Cookies'
+		btnFortune.textContent = 'Random'
+
+		// ClassName
+		containerFortune.className = 'container__fortune'
+		containerRow.className = 'fortune-row'
+		btnFortune.className = 'heading-close-button'
+
+		// Add UI
+		containerFortune.append(containerRow, randomTitle)
+		containerRow.append(titleFortune, btnFortune)
+		document.body.appendChild(containerFortune)
+
+		const data = await this.fetchFortuneData()
+
+		btnFortune.addEventListener('click', e => {
+			const { target } = e
+			if (data.length > 0 && target) {
+				const randomId = random(0, data.length - 1)
+				const selectedItem = data[randomId]
+
+				randomTitle.id = randomId
+				randomTitle.style.color = `rgb(${random(0, 255)}, ${random(
+					0,
+					255
+				)}, ${random(0, 255)})`
+				randomTitle.textContent = selectedItem.title
+			}
+		})
+	}
 }
