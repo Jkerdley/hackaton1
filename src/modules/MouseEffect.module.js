0dd53cfb3ed00e83dd7adbd500c88682b90a9853
Berezin Pavel <pas.1999@mail.ru>
2024-08-18 19:30:00 +0700

    add function in mouseEffect.js

--- src/modules/MouseEffect.module.js
+++ src/modules/MouseEffect.module.js
@@ -1,13 +1,9 @@
 import { Module } from "../core/module";
 import { getRandomColor, random } from "../utils";
 
-const effects = [cirlce, buble, square];
-// const effects = [square];
+const effects = [cirlce, buble, spark];
 
 export class MouseEffectModule extends Module {
-  // constructor() {
-  //   super("mouse-effect", "Добавить случайный эфект мышке");
-  // }
   trigger() {
     document.body.removeEventListener("mousemove", this.func);
     this.func = this.mousemoveHandler();
@@ -21,20 +17,10 @@ export class MouseEffectModule extends Module {
 }
 
 function cirlce(e) {
-  // console.log(e);
-
-  const figure = document.createElement("div");
-  figure.style.cssText =
-    "border-radius: 50%; transition: .5s; transform: scale(2)";
-  figure.style.width = "10px";
-  figure.style.height = "10px";
-  figure.style.backgroundColor = getRandomColor();
-  figure.style.position = "absolute";
-  figure.style.top = `${e.clientY}px`;
-  figure.style.left = `${e.clientX}px`;
+  const figure = creeateFigure(e.clientX, e.clientY);
+  figure.style.borderRadius = "50%";
+  figure.style.transform = " scale(2)";
   document.body.append(figure);
-
-  // figure.style.cssText = "transform: scale(10)";
 
   setTimeout(() => {
     figure.style.transform = "scale(10)";
@@ -47,21 +33,12 @@ function cirlce(e) {
 }
 
 function buble(e) {
-  // console.log(this);
-
-  const figure = document.createElement("div");
-  figure.style.cssText =
-    "border-radius: 50%; transition: .5s; transform: translateY(0px)";
-  figure.style.width = "10px";
-  figure.style.height = "10px";
-  figure.style.backgroundColor = getRandomColor();
-  figure.style.position = "absolute";
-  figure.style.top = `${e.clientY}px`;
-  figure.style.left = `${e.clientX}px`;
+  const figure = creeateFigure(e.clientX, e.clientY);
+  figure.style.borderRadius = "50%";
+  figure.style.transform = " translateY(0px)";
   document.body.append(figure);
 
   setTimeout(() => {
-    // figure.style.transform = `translateY(100%)`;
     figure.style.top = "100%";
     figure.style.opacity = ".5";
 
@@ -71,28 +48,33 @@ function buble(e) {
   }, 100);
 }
 
-function square(e) {
-  const figure = document.createElement("div");
-  figure.style.cssText = " transition: .5s; ";
+function spark(e) {
+  const figure = creeateFigure(e.clientX, e.clientY);
   figure.style.transform = `translate(
     ${random(-10, 10)}px, ${random(-10, 10)}px)`;
 
-  figure.style.width = "10px";
-  figure.style.height = "10px";
-  figure.style.backgroundColor = getRandomColor();
-  figure.style.position = "absolute";
-  figure.style.top = `${e.clientY}px`;
-  figure.style.left = `${e.clientX}px`;
   document.body.append(figure);
 
   setTimeout(() => {
+    figure.style.opacity = ".5";
     figure.style.transform = `translate(
       ${random(-50, 50)}px, ${random(-50, 50)}px)`;
-
-    figure.style.opacity = ".5";
 
     setTimeout(() => {
       figure.remove();
     }, 1000);
   }, 100);
+}
+
+function creeateFigure(x, y) {
+  const figure = document.createElement("div");
+  figure.style.cssText =
+    "border-radius: 50%; transition: .5s; transform: scale(2)";
+  figure.style.width = "10px";
+  figure.style.height = "10px";
+  figure.style.backgroundColor = getRandomColor();
+  figure.style.position = "absolute";
+  figure.style.top = `${y}px`;
+  figure.style.left = `${x}px`;
+  return figure;
 }
